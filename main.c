3a95c0c2db1354e88547369c8c114ffd7d054c85
OmarSha3rawy <omar.aadel58@gmail>
2019-11-07 17:16:03 +0200

    first files

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * ADC_switch.c
+ *
+ * Created: 11/7/2019 4:30:36 PM
+ * Author : omara
+ */ 
+
+#include <avr/io.h>
+#include "TIMER.h"
+
+int main(void)
+{
+	TIMER_init(TIMER0);
+	ADC_set_callback
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
