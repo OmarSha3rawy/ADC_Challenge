e38c41ad2502d60d182cbfd5987cfd4c68cde8d9
OmarSha3rawy <omar.aadel58@gmail>
2019-11-08 17:54:26 +0200

    first release

---
+++ [req4] Fan control/[req4] Fan control/ADC_PbCfg.h
@@ -0,0 +1,15 @@
+/*
+ * ADC_PbCfg.h
+ *
+ *  Created on: Nov 7, 2019
+ *      Author: Mohammed Nassar
+ */
+
+#ifndef ADC_PBCFG_H_
+#define ADC_PBCFG_H_
+
+#include "ADC.h"
+
+extern const ADC_cfg_t ADC_cfg;
+
+#endif /* ADC_PBCFG_H_ */
